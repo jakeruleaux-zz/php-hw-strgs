eb0aba7385d3f89ad62970e207dba7f882b30df7
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-25 14:45:58 -0700

    builds php template form

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $upper_text = $_GET["upper_text"];
+    $counts = $_GET["counts"];
+    $shuffle = $_GET["shuffle"];
+    $pop = $_GET["pop"];
+    $upper_text = strtoupper($upper_text);
+    $counts = str_word_count($counts);
+    $shuffle = str_shuffle($shuffle);
+    $pop = stripos($pop);
+
+?>
+<html>
+<head>
+    <title>String doing things</title>
+</head>
+<body>
+    <h1>Game on:</h1>
+    <p><?php echo $upper_text; ?> </p>
+    <p><?php echo $counts; ?> </p>
+    <p><?php echo $shuffle; ?> </p>
+    <p><?php echo $pop; ?> </p>
+</body>
+</html>
